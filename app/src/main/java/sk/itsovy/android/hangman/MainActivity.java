29c88e463d8a1d5b913990135e5c228da8eadbe3
miroslav-opiela <miroslav-opiela@upjs.sk>
2020-09-25 10:45:03 +0200

    funkcna hra - color filter, vitazstvo, prehra

--- app/src/main/java/sk/itsovy/android/hangman/MainActivity.java
+++ app/src/main/java/sk/itsovy/android/hangman/MainActivity.java
@@ -2,6 +2,9 @@ package sk.itsovy.android.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -19,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private EditText editText;
 
+    private final int[] gallowsIds = {
+            R.drawable.gallows0,
+            R.drawable.gallows1,
+            R.drawable.gallows2,
+            R.drawable.gallows3,
+            R.drawable.gallows4,
+            R.drawable.gallows5,
+            R.drawable.gallows6,
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +41,15 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textViewGuessedWord);
         editText = findViewById(R.id.editTextLetter);
 
-        hangmanGame = new HangmanGame(new Random());
+        restartGame();
     }
 
     public void onGallowsClick(View view) {
+        if (hangmanGame.isWon() || hangmanGame.getAttemptsLeft() == 0) {
+            restartGame();
+            return;
+        }
+
         CharSequence text = editText.getText();
         if (text == null || text.length() == 0) {
             Toast.makeText(this, "Insert a letter",
@@ -48,7 +67,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        hangmanGame.guess(letter);
+        boolean success = hangmanGame.guess(letter);
         editText.setText("");
+
+        if (success) {
+            textView.setText(hangmanGame.getGuessedCharacters());
+            if (hangmanGame.isWon()) {
+                alertWonGame();
+            }
+        } else {
+            int gallowsIdx = Game.DEFAULT_ATTEMPTS_LEFT - hangmanGame.getAttemptsLeft();
+            imageView.setImageResource(gallowsIds[gallowsIdx]);
+
+            if (hangmanGame.getAttemptsLeft() == 0) {
+                // PREHRA
+                alertLostGame();
+                textView.setText(hangmanGame.getChallengeWord());
+            }
+        }
+    }
+
+    private void alertLostGame() {
+        ColorFilter filter = new LightingColorFilter(Color.RED, Color.BLACK);
+        imageView.setColorFilter(filter);
+    }
+
+    private void alertWonGame() {
+        ColorFilter filter = new LightingColorFilter(Color.GREEN, Color.BLACK);
+        imageView.setColorFilter(filter);
+
+    }
+
+    private void restartGame() {
+        hangmanGame = new HangmanGame(new Random());
+        textView.setText(hangmanGame.getGuessedCharacters());
+        imageView.setColorFilter(null);
+        imageView.setImageResource(gallowsIds[0]);
     }
 }
